20c4b4712dc9719ab842deed3c0d22d666a80446
kmadhav907 <kmadhav907@gmail.com>
2021-03-06 20:22:13 +0530

    Completed 7 Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,67 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final player = AudioCache();
+    void playSound(int number) {
+      player.play('note$number.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
+            child: Column(
+          children: [
+            FlatButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                color: Colors.pink,
+                child: Text("SA")),
+            FlatButton(
               onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
+                playSound(2);
               },
-              child: Text("Click Me"),
+              child: Text("RE"),
+              color: Colors.red,
             ),
-          ),
-        ),
+            FlatButton(
+              onPressed: () {
+                playSound(3);
+              },
+              child: Text("GA"),
+              color: Colors.yellowAccent,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(4);
+              },
+              child: Text("MA"),
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(5);
+              },
+              child: Text("PA"),
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(6);
+              },
+              child: Text("DA"),
+              color: Colors.deepOrangeAccent,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound(7);
+              },
+              child: Text("NI"),
+              color: Colors.purple,
+            )
+          ],
+        )),
       ),
     );
   }
